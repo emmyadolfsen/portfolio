ca10042c64b8121ed72aabc17aa32acb526860ca
Emmy Adolfsén <emmy.adolfsen@gmail.com>
2020-10-19 12:54:08 +0200

    second last commit

--- src/js/projects.js
+++ src/js/projects.js
@@ -17,7 +17,7 @@ function getProjects() {
                     `
                     <a href="${object.project_url}">
                 <li class="project-list">
-                <img src="/images/${object.project_img}" class="project_img" alt="">
+                <img src="images/${object.project_img}" class="project_img" alt="">
                 <h2 class="project-text">${object.project_name}</h2>
                     <p>${object.project_d}</p>
                 </li></a>
